4332fff38dfda14a7d30b66e6aacaebe4afbcb64
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-01 17:53:22 +0100

    add minestone 2

--- minestone_2/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* Seconda Milestone:
-Attraverso l’utilizzo di axios:
-al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi  e li stamperà attraverso vue. */
---
+++ minestone_2/js/script.js
@@ -0,0 +1,32 @@
+console.log("Vue ok", Vue);
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        albums: [],
+        search: "",
+    },
+    methods: {
+        searchAlbums() {
+            axios
+                .get("http://localhost/php-ajax-dischi/minestone_2/api/data.php")
+                .then((res) => {
+                    this.albums = res.data;
+                    console.log(this.search);
+                    console.log(this.albums);
+                    if (this.search !== "") {
+                        this.albums = this.albums.filter((album) => {
+                            return album.title
+                                .toLowerCase()
+                                .includes(this.search.toLowerCase());
+                        });
+                    }
+                });
+        },
+    },
+    mounted() {
+        this.searchAlbums();
+    },
+});
